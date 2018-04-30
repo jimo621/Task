b52a4735def2608b4131903bdd1eb5b746bd63b4
指缝de阳光 <34292955+jimo621@users.noreply.github.com>
2018-04-30 19:52:20 +0800

    Add files via upload
    task2

---
+++ Java/hanzhongwei/task1/Task2/src/main/java/hzw/service/impl/StudentServiceImpl.java
@@ -0,0 +1,46 @@
+package hzw.service.impl;
+
+import hzw.mapper.StudentMapper;
+import hzw.model.Student;
+import hzw.service.StudentSerivce;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentSerivce{
+
+    @Autowired
+    StudentMapper studentMapper;
+
+    @Override
+    public List<Student> getAll() {
+        return studentMapper.getAll();
+    }
+
+    @Override
+    public void addStu(Student student) {
+        student.setCreate_at(System.currentTimeMillis());
+        student.setUpdate_at(System.currentTimeMillis());
+        studentMapper.addStu(student);
+    }
+
+    @Override
+    public void deleteStu(long stuId) {
+        studentMapper.deleteStu(stuId);
+    }
+
+    @Override
+    public void updateStu(Student student) {
+        System.out.println(student.toString());
+        student.setUpdate_at(System.currentTimeMillis());
+        studentMapper.updateStu(student);
+    }
+
+    @Override
+    public Student getId(long stuId) {
+        System.out.println("进来没");
+        return studentMapper.getId(stuId);
+    }
+}
